a42ba55e0d90f676ef8c9f06f57744a6494f1da6
vijay-kannan <37103145+vijay-kannan@users.noreply.github.com>
2019-07-02 16:13:04 +0530

    Create number.py

---
+++ number.py
@@ -0,0 +1,13 @@
+z1=int(input())
+e1=[]
+for i in range(0,z1):
+    inpu=input()
+    e1.append(inpu)
+ci=[]
+for i in zip(*e1):
+    if(i.count(i[0])==len(i)):
+        ci.append(i[0])
+            
+    else:
+        break
+print(''.join(ci))
