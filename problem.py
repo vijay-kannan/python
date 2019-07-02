14f034362504f5c19adc00a5b839d0e60767ec2d
vijay-kannan <37103145+vijay-kannan@users.noreply.github.com>
2019-07-02 16:25:44 +0530

    Update problem.py

--- problem.py
+++ problem.py
@@ -3,10 +3,10 @@ sr=0
 if len(vv1)>len(vkk):
   vv1,vkk=vkk,vv1
 p=0
-while p<len(vv1):
-  sr+=(ord(vkk[p])-ord(vv1[p]))
-  p+=1
-for p in range(p,len(vkk)):
-  sr+=ord(vkk[p])-ord('a')+1
+while q<len(vv1):
+  sr+=(ord(vkk[q])-ord(vv1[q]))
+  q+=1
+for q in range(q,len(vkk)):
+  sr+=ord(vkk[q])-ord('a')+1
 print(sr)
 © 2019 GitHub, Inc.
