573458516fc1dc72bdfeaff7e982d176fadb2ef1
vijay-kannan <37103145+vijay-kannan@users.noreply.github.com>
2019-07-02 15:46:11 +0530

    Create vowelorconstant.py

---
+++ vowelorconstant.py
@@ -0,0 +1,13 @@
+b=input()
+if b == "A" or b == "a":
+	print("Vowel")
+elif b == "E" or b == "e":
+	print("Vowel")
+elif b == "I" or b == "i":
+	print("Vowel")
+elif b == "O" or b == "o":
+	print("Vowel")
+elif b == "U" or b == "u":
+	print("Vowel")
+else:
+	print("Consonant")
